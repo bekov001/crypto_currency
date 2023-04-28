18bbfab1a93237d4fd818546841e2a04682bca11
Anuar <ava200ava@gmail.com>
2023-04-28 12:29:37 +0600

    udate123

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/cyrpto_currency/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/crypto_currency/" : "/",
   };
